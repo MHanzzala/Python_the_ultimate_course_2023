000510deff10af90174351172df4cc4d0937e1b6
hhh <hanzalakhokhar@gmail.com>
2023-02-04 10:53:13 +0500

    v1

--- 6. Organizing Code With Python Functions/3_Function_return.py
+++ 6. Organizing Code With Python Functions/3_Function_return.py
@@ -1,14 +1,10 @@
 
 def pwr2(num):
-    pw2= num*num
+    pw2 = num*num
     div = num/num
     return pw2, div
-  
-    
 
 
-pw2, div= pwr2(5)
+pw2, div = pwr2(25)
 print("pw2 is ", pw2)
 print("div is ", div)
-
-
