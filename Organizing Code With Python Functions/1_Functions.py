a3a3fa583176db732231748d38467e017b048015
hhh <hanzalakhokhar@gmail.com>
2023-02-04 10:31:20 +0500

    v1

---
+++ Organizing Code With Python Functions/1_Functions.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+ 
+"""
+
+db1=["Kate", "Moss", "David"]
+db2=["Lee", "Steve"]
+db3=["Tony", "Lara"]
+
+
+
+def addToDatabase(name): 
+   
+    global db1
+    global db2
+    global db3
+    
+    db1=[name]
+    db2=[name]
+    db3=[name]
+    
+
+    
+    
+    
+
+
+
+
+addToDatabase("John")
+addToDatabase("MrHamsho")
+print("Db 1 is: ", db1)
+print("Db 2 is: ", db2)
+print("Db 3 is: ", db3)
---
+++ Organizing Code With Python Functions/3_Function_return.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+ 
+"""
+
+def pwr2(num):
+    pw2= num*num
+    div = num/num
+    return pw2, div
+  
+    
+
+
+pw2, div= pwr2(5)
+print("pw2 is ", pw2)
+print("div is ", div)
+
+
