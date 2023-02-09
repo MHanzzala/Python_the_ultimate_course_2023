4d4773387fa5f76a6586e04cc2f4faeeefc4b0c7
hhh <hanzalakhokhar@gmail.com>
2023-02-09 15:31:43 +0500

    v1

--- 13. ReadWrite ZipUnzip Files/2_WriteFile.py
+++ 13. ReadWrite ZipUnzip Files/2_WriteFile.py
@@ -5,8 +5,14 @@ with open('writeThisNow.txt', 'w') as f:
 
     f.writelines('\n'.join(lines))
 
+#append the line
+lines = ['\n', "This the line_one", "This is line_2","And this is line_3"]
 
-lines = ['\n', "new line 1", "new line 2"]
+with open('writeThisNow.txt', 'a') as f:
+    f.writelines('\n'.join(lines))
+
+#again using the 'w' that means it gonna rewrite all the lines
+lines = ['\n', "blah blah blah", "helloG"]
 
 with open('writeThisNow.txt', 'w') as f:
     f.writelines('\n'.join(lines))
