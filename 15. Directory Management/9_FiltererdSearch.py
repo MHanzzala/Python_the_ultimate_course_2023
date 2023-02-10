c9991aea46ba2757e309ed94f9ac6b6ba1da6c3b
hhh <hanzalakhokhar@gmail.com>
2023-02-10 12:14:59 +0500

    v1

--- 15. Directory Management/9_FiltererdSearch.py
+++ 15. Directory Management/9_FiltererdSearch.py
@@ -2,19 +2,16 @@
 import os
 
 dirList = os.listdir()
-#dirList = [x for x in dirList if not x.endswith('jpg')]
+# dirList = [x for x in dirList if not x.endswith('jpg')]
 print(dirList)
-baseName= "data"
+baseName = "data"
 extension = ".jpg"
 
-for i,img in enumerate(dirList):
-    
-    
-        imgName = baseName + str(i) + extension
-     
-        try:
-            os.rename(img, imgName)
-        except OSError as error:
-            print(error)
+for i, img in enumerate(dirList):
 
- 
+    imgName = baseName + str(i) + extension
+
+    try:
+        os.rename(img, imgName)
+    except OSError as error:
+        print(error)
