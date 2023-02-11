667beeef7fcbe38f7175b36ef6f01f3e16a73bf2
hhh <hanzalakhokhar@gmail.com>
2023-02-11 13:24:03 +0500

    v1

--- 16. Building Command Line Applications/2_CLIapp.py
+++ 16. Building Command Line Applications/2_CLIapp.py
@@ -12,7 +12,7 @@ args = my_parser.parse_args()
 input1 = int(args.arg1)
 input2 = int(args.arg2)
 
-print("The Summation is:", input1+input2)
+print("The Addition is:", input1+input2)
 print("The Subtraction is:", input1-input2)
 print("The Multiplication is:", input1*input2)
 print("The Division is:", input1/input2)
