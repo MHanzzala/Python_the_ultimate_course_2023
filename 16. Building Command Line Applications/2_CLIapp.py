dc360d53c57ac03b299214615d62f9888072809e
hhh <hanzalakhokhar@gmail.com>
2023-02-11 13:22:39 +0500

    v1

--- 16. Building Command Line Applications/2_CLIapp.py
+++ 16. Building Command Line Applications/2_CLIapp.py
@@ -1,7 +1,8 @@
 import argparse
 
 
-my_parser = argparse.ArgumentParser(prog="app", fromfile_prefix_chars='@',description='Returns the Addition, multiplication, division and subtraction of any two numbers')
+my_parser = argparse.ArgumentParser(prog="app", fromfile_prefix_chars='@',
+                                    description='Returns the Addition, multiplication, division and subtraction of any two numbers')
 
 my_parser.add_argument('arg1', help='The First Entry')
 my_parser.add_argument('arg2', help='The Second Entry')
