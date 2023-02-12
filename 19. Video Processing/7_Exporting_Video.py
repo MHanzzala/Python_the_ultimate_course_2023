7f08cab0efc49635adffdd884a68fd8d73061dd8
hhh <hanzalakhokhar@gmail.com>
2023-02-12 19:25:27 +0500

    v1

--- 19. Video Processing/7_Exporting_Video.py
+++ 19. Video Processing/7_Exporting_Video.py
@@ -29,7 +29,7 @@ width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 ex = cv2.VideoWriter_fourcc(*'MP4V')
-out = cv2.VideoWriter('Media/logoEdited.mp4', ex, 20, (width, height))
+out = cv2.VideoWriter('Media/Logo_video_edited.mp4', ex, 20, (width, height))
 
 while (cap.isOpened()):
     ret, frame = cap.read()
