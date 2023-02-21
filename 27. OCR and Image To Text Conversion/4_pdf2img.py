edf9b862222284aa3fe626b50e9efe0362a21dcd
hhh <hanzalakhokhar@gmail.com>
2023-02-21 11:09:58 +0500

    v1

--- 27. OCR and Image To Text Conversion/4_pdf2img.py
+++ 27. OCR and Image To Text Conversion/4_pdf2img.py
@@ -4,11 +4,11 @@ import cv2
 from pdf2image import convert_from_path
 
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = r"/home/hanzala/Development/Python_the_ultimate_course_2023/27. OCR and Image To Text Conversion/tesseract-ocr-w64-setup-5.3.0.20221222.exe"
 
 
 img = convert_from_path(
-    r"/home/hanzala/Development/Python_the_ultimate_course_2023/27. OCR and Image To Text Conversion/invoice1.pdf")
+    r"/home/hanzala/Development/Python_the_ultimate_course_2023/27. OCR and Image To Text Conversion/Invoice1.pdf")
 img = img[0]
 
 
